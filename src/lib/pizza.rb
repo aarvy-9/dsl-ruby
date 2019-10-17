1952eddebbedfaf527860680e119c1abaea7dc57
rahulv2901@gmail.com <rahulv2901@gmail.com@1e9af996-6bc9-4e4b-9a57-10da00564793>
2019-10-17 02:10:07 +0000

    Removed unused code
    git-svn-id: https://agile.cs.uh.edu/svn/f19_proglangs/jella_vempati/assign3/Ruby@2115 1e9af996-6bc9-4e4b-9a57-10da00564793

--- src/lib/pizza.rb
+++ src/lib/pizza.rb
@@ -1,29 +1,13 @@
 class Pizza
-  attr_accessor :name, :spreads, :alltoppings, :bake
+  attr_accessor :name
 
   def initialize(&block)
-	self.spreads = []
-
 	instance_eval &block
-  end
-  
-  def spread(*args)
-	spreads << args[0]
-  end
-  
-  def toppings(*args)
-	@alltoppings = args
-  end
-  
-  def create(*args)
-	if args.length() == 1
-	  @name = args[0]
-	end
   end
   
   def method_missing(name, *args)
 	if args.length() == 1
-	  return args[0]
+	  @name = args[0]
 	end
   end
 end
