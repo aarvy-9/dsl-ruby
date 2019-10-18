c29935868cb0b1a704615a8d0d3b490184eceb28
svenkat@cs.uh.edu <svenkat@cs.uh.edu@1e9af996-6bc9-4e4b-9a57-10da00564793>
2019-10-18 13:00:38 +0000

    assign3 synopsis:  let's get the DSL structure in place
    git-svn-id: https://agile.cs.uh.edu/svn/f19_proglangs/jella_vempati/assign3/Ruby@2162 1e9af996-6bc9-4e4b-9a57-10da00564793

--- test/TestCreatePizza.rb
+++ test/TestCreatePizza.rb
@@ -4,9 +4,13 @@ require 'test/unit'
 class TestCreatePizza < Test::Unit::TestCase
 	def test_canary
 		assert_equal(TRUE, TRUE)
+		#Feedback: assert true
 	end
 
 	def test_createpizza
 		assert_equal(CreatePizza(), Pizza)
+		#Feedback: Let's get the DSL structure in place
+		#result = process('create Pizza')
+		#assert_equal 'Pizza:', result
 	end
 end
