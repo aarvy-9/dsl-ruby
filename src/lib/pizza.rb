8b9c11edda702d55f72d83d9cf1023c22e04b239
hariprasad.jella05@gmail.com <hariprasad.jella05@gmail.com@1e9af996-6bc9-4e4b-9a57-10da00564793>
2019-10-20 21:42:07 +0000

    Added spread, toppings functions
    git-svn-id: https://agile.cs.uh.edu/svn/f19_proglangs/jella_vempati/assign3/Ruby@2252 1e9af996-6bc9-4e4b-9a57-10da00564793

--- src/lib/pizza.rb
+++ src/lib/pizza.rb
@@ -1,7 +1,22 @@
 class Pizza
-  
+	
 	def create(arg)
 		arg.to_s.concat(":")
 	end
 	
+	def spread(arg)
+		arg
+	end
+	
+	def toppings(*args)
+		puts *args
+	end
+	
+	def method_missing(m, *args, &block)
+		if m.to_s == "cheese" or m.to_s == "sauce"
+			m.to_s.concat(":")
+		elsif
+			puts m
+		end
+	end
 end
--- test/TestCreatePizza.rb
+++ test/TestCreatePizza.rb
@@ -6,8 +6,42 @@ class TestCreatePizza < Test::Unit::TestCase
 		assert true
 	end
 
-	def test_createpizza
-		result = process('create Pizza')
+	def test_create_pizza
+		input_string = <<-INPUT
+							create Pizza
+						INPUT
+		result = process(input_string)
 		assert_equal 'Pizza:', result
 	end
+	
+	def test_spread_cheese
+		input_string = <<-INPUT
+							create Pizza
+							spread cheese
+						INPUT
+		result = process(input_string)
+		assert_equal 'cheese:', result
+	end
+	
+	def test_spread_sauce
+		input_string = <<-INPUT
+							create Pizza
+							spread cheese
+							toppings onions, jalapenos, green_pepper
+						INPUT
+		result = process(input_string)
+		assert_equal "onions: jalapenos: green_pepper: ", result
+	end
+	
+	def test_spread_sauce
+		input_string = <<-INPUT
+							create Pizza
+							spread cheese
+							toppings onions, jalapenos, green_pepper
+							spread sauce
+						INPUT
+		result = process(input_string)
+		assert_equal 'sauce:', result
+	end
+
 end
